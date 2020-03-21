897727cc27421cae0b327388e5370d14442cb78b
feelsever <philipp.bhandari@gmail.com>
2020-03-21 11:20:45 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Volvo</title>
+
+<!-- JQUERY -->
+<script
+src="https://code.jquery.com/jquery-2.2.4.min.js"
+integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+crossorigin="anonymous"></script>
+
+<!-- CSS -->
+<link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+<link rel="stylesheet" href="static/css/main.css">
+
+<!-- SCRIPTS -->
+<script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+<script src="static/main.js"></script>
+
+<script>
+    $(document).ready(function() {
+        __COMMON.commonFunctions();
+
+    });
+</script>
+</head>
+
+<body>
+<?php
+    include 'templates/block_about_company.html';
+    include 'templates/block_services.html';
+    include 'templates/block_models.html';
+    include 'templates/block_contacts.html';
+?>
+
+</body>
+</html>
